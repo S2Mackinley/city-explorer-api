70b14542ade129065218d98a22d1446f30fcab51
Elijah Prom <elijahprom@gmail.com>
2021-03-23 21:23:39 -0700

    error with removing a folder is fixed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const cors = require('cors');
 
-const weather = require('./data/weather.json');
+const weather = require('./weather.json');
 
 const app = express();
 
