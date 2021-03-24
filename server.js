a86c8c2a92d18163c224b05b3519e14cf8a36b9d
Elijah Prom <elijahprom@gmail.com>
2021-03-23 21:13:14 -0700

    fixed server issues

--- server.js
+++ server.js
@@ -1,51 +1,49 @@
 'use strict';
 
-// bring in the express libraray
-// don't forget to do an npm install express
 const express = require('express');
-//allows us to access our env variables
 require('dotenv').config();
-//allow our front-end to access our server
+
 const cors = require('cors');
-// initalizing the express library so I can use it
+
+const weather = require('./data/weather.json');
+
 const app = express();
-//weather
-const weather = require('./weather.json');
-//this allows anyone to access our server - aka - the worlds worst body guard
+
 app.use(cors());
-const PORT = process.env.PORT || 3001;
+
+const PORT = process.env.PORT;
+
 app.get('/', function (request, response) {
-    response.send('Hello World');
-});
-
-
-
+    response.send('Hello World')
+})
 
 app.get('/weather', handleWeather);
 
-function handleWeather(request, response) {
-  let forecastArray = weather.data.map(day => {
-    return new Forecast(day,weather.city_name,weather.lat,weather.lon)
-  })
-  response.status(200).send(forecastArray)
-
+function handleWeather(request,response){
+  const city = weather.city_name;
+  const lat = weather.lat;
+  const lon = weather.lon;
+  
+  try{
+    let array = weather.data.map(day => {
+      return new Forecast(day)
+      });
+      const results = {
+        city: city,
+        lat: lat,
+        lon: lon,
+        forecast: array
+      };
+    response.status(200).json(results);
+  } catch (error) {
+    response.status(500).send(error.message);
+}
+console.log(error)
 }
 
-function Forecast(obj, city, lat, lon) {
-  this.description = obj.weather.description;
-  this.date = obj.datetime;
-  this.city = city;
-  this.lat = lat;
-  this.lon = lon;
+function Forecast(obj){
+    this.description = obj.weather.description;
+    this.date = obj.datetime; 
 }
 
-
-
-// turn on the server
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
-
-console.log
-// three ways to do it:
-// 1. node server.js
-// 2. npm start
-// 3. nodemon - this is going to check for changes and update
